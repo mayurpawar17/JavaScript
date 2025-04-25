89006b99067d1bc925f6c542f899ad577496670c
Mayur Pawar <144547668+mayurpawar17@users.noreply.github.com>
2025-04-25 19:11:44 +0000

    datatype summary info

---
+++ 01 Basic/datatypes-summary.js
@@ -0,0 +1,26 @@
+// Primitive datatype ->  String, Number, Boolean, null, undefined, symbol, BigInt
+
+let fName = "mayur";
+let age = 22;
+let isDark = true;
+let outsideTemp = null;
+let bigInt = 51515515154151545415154; //BigInt
+
+// non-Primitive datatype (reference)  -> Array, Objects, Functions
+
+//array
+let pokemons = ["pikachu", "bulbasour", "goldduck"];
+
+//objects (key value pair)
+
+let personInfo = {
+  name: "mayur",
+  age: 22,
+  mobile: 942814652,
+};
+
+//function
+
+const myFunction = function () {
+  console.log("Hello world");
+};
