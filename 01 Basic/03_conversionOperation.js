f1c60b8963fe23aeb9a90a6f0afc0c4506db4a65
Mayur Pawar <144547668+mayurpawar17@users.noreply.github.com>
2025-04-25 18:49:09 +0000

    comparsion exercise

--- 01 Basic/03_conversionOperation.js
+++ 01 Basic/03_conversionOperation.js
@@ -1,25 +1,53 @@
+//Conversion
+
 let score = 33;
 
 let valueInNumber = Number(score);
 
-console.log(typeof score); //number
-
+// console.log(typeof score); //number
 
 // "33" -> 33
 // "33abc" -> NaN
 // ture -> 1; false -> 0
 
-let isLogedIn=1;
-let booleanIsLoggdIn= Boolean(isLogedIn);
-console.log(typeof booleanIsLoggdIn); //boolean
-
+let isLogedIn = 1;
+let booleanIsLoggdIn = Boolean(isLogedIn);
+// console.log(typeof booleanIsLoggdIn); //boolean
 
 // 1 -> ture; 0 -> false;
 //"" -> false
 //"mayur" -> ture
 
+let nubmer = 33;
+let numberInString = String(nubmer);
+// console.log(typeof numberInString); // string
 
-let nubmer=33;
-let numberInString= String(nubmer);
-console.log(typeof numberInString); // string 
+//Operation
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " world";
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter=100;
+gameCounter++;
+
+console.log(gameCounter); //101
 
