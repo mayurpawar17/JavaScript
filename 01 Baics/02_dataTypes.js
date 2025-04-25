f6e1b08dfaa73453d19b804efeaafbfe855a8d7f
Mayur Pawar <144547668+mayurpawar17@users.noreply.github.com>
2025-04-25 14:05:32 +0000

    datatype info

---
+++ 01 Baics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version\
+
+// alert(20+30) //we are using node.js, not browser
+
+console.log(3+3);
+
+
+
+//premitive datatypes
+
+//number -> range 2 to power 53
+//bigint 
+//string 
+//boolean -> ture/false
+//null -> standalone value or empty
+//undefined -> 
+//symbol -> unique
+
+let fname="mayur";
+let age=22;
+
+console.log(typeof fname); // output -> String
+console.log(typeof age); // output -> number
+
+
+
+
