ce4a58e08b79004bb0dee1d307bc163534c29e0e
Mayur Pawar <144547668+mayurpawar17@users.noreply.github.com>
2025-04-25 14:23:15 +0000

    conversion operation with datatype

---
+++ 01 Basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = 33;
+
+let valueInNumber = Number(score);
+
+console.log(typeof score); //number
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// ture -> 1; false -> 0
+
+let isLogedIn=1;
+let booleanIsLoggdIn= Boolean(isLogedIn);
+console.log(typeof booleanIsLoggdIn); //boolean
+
+
+// 1 -> ture; 0 -> false;
+//"" -> false
+//"mayur" -> ture
+
+
+let nubmer=33;
+let numberInString= String(nubmer);
+console.log(typeof numberInString); // string 
+
